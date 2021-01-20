c91c1ed0e824545b40d6837158bf02aa57a1ab8c
CAUΛ <cauadev@gmail.com>
2021-01-20 01:40:01 -0300

    Criando o modelo produto

---
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -0,0 +1,69 @@
+package com.produtos.apirest.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	private double valor;
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+	
+	
+	
+
+}
