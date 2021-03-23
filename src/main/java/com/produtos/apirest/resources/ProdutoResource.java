8c9630112a99bfb05763f21452caab91d7014c0d
CAUΛ <cauadev@gmail.com>
2021-03-23 16:02:49 -0300

    Update ProdutoResource.java

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -24,7 +24,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value = "/api")
 @Api(value = "API REST produtos")
-@CrossOrigin(origins = "http://localhost:8080/")
+@CrossOrigin(origins = "**")
 public class ProdutoResource {
 	
 	@Autowired
